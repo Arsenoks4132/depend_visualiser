743931c4a43cc4116ebcd9ae4d60c4afa238a1f9
Arsenoks4132 <Danov2005@yandex.ru>
2024-09-27 13:35:56 +0300

    added getting xml from url

--- graph_builder.py
+++ graph_builder.py
@@ -1,11 +1,26 @@
 from requests import get
-
+import xml.etree.ElementTree as ET
+from re import sub
 
 class GraphBuilder:
     def __init__(self):
-        self.xml = None
         pass
 
+    def build_graph(self, group, artifact, version):
+        pass
+
+
+
     def get_pom(self, href):
-        self.xml = get(href)
-        print(self.xml.text)
+        xml = get(href)
+        with open('temp.xml', 'wt') as file:
+            text = xml.text
+            text = sub(' xmlns="[^"]+"', '', text, count=1)
+            file.write(text)
+
+        root = ET.parse('temp.xml').getroot()
+        for dp in root.findall('dependencies/dependency'):
+            group = dp.find('groupId').text
+            artifact = dp.find('artifactId').text
+            version = dp.find('version').text
+            print(group, artifact, version)
