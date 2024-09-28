bd4b2717efb4dd572256873f56e868b855105219
Arsenoks4132 <Danov2005@yandex.ru>
2024-09-28 10:43:01 +0300

    added main functional

--- graph_builder.py
+++ graph_builder.py
@@ -1,26 +1,86 @@
 from requests import get
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as et
 from re import sub
+from os import listdir
+
 
 class GraphBuilder:
     def __init__(self):
-        pass
+        self.href = 'https://repo1.maven.org/maven2/'
+        self.start = True
+        self.lines = set()
 
-    def build_graph(self, group, artifact, version):
-        pass
+    def build_url(self, package: dict[str, str]):
+        url = (f"{self.href}{'/'.join(package['group'].split('.'))}/"
+               f"{package['artifact']}/{package['version']}/"
+               f"{package['artifact']}-{package['version']}.pom")
+        return url
 
+    def load_xml(self, package: dict[str, str]):
+        f_name = f"{package['artifact']}_{package['version']}.xml"
 
+        if f_name not in listdir('cache'):
+            url = self.build_url(package)
+            package_pom = get(url)
+            if package_pom.status_code == 200:
+                with open(f'cache/{f_name}', 'wt') as file:
+                    text = sub(' xmlns="[^"]+"', '', package_pom.text, count=1)
+                    file.write(text)
+            else:
+                print('Не удалось открыть репозиторий пакета')
+                return
 
-    def get_pom(self, href):
-        xml = get(href)
-        with open('temp.xml', 'wt') as file:
-            text = xml.text
-            text = sub(' xmlns="[^"]+"', '', text, count=1)
-            file.write(text)
+        return et.parse(f'cache/{f_name}').getroot()
 
-        root = ET.parse('temp.xml').getroot()
-        for dp in root.findall('dependencies/dependency'):
-            group = dp.find('groupId').text
-            artifact = dp.find('artifactId').text
-            version = dp.find('version').text
-            print(group, artifact, version)
+    def build_graph(self, code: str, package: dict[str, str]):
+        graph_code = ('@startuml\n\n\n' +
+                      self.__build_graph(package) +
+                      '\n\n@enduml')
+        with open(code, 'wt') as file:
+            file.write(graph_code)
+
+        self.lines = set()
+
+    def __build_graph(self, package: dict[str, str]):
+        pom = self.load_xml(package)
+        line = f"{package['artifact']}: {package['version']}\n\n".replace('-', '_')
+        graph_code = ''
+        if line not in self.lines:
+            graph_code = line
+            self.lines.add(line)
+
+        for dp in pom.findall('dependencies/dependency'):
+            pkg = dict()
+
+            optional = dp.find('optional')
+            if optional is not None:
+                if optional.text == 'true':
+                    continue
+
+            pkg['group'] = dp.find('groupId').text
+            pkg['artifact'] = dp.find('artifactId').text
+            pkg['version'] = dp.find('version').text
+
+            art_from = package['artifact'].replace('-', '_')
+            art_to = pkg['artifact'].replace('-', '_')
+
+            line = f"{art_from} -down-> {art_to}\n\n"
+            if line not in self.lines:
+                graph_code += line
+                self.lines.add(line)
+
+            graph_code += self.__build_graph(pkg)
+
+        return graph_code
+
+    # def get_pom(self, href):
+    #     xml = get(href)
+    #     with open('temp.xml', 'wt') as file:
+    #         text = sub(' xmlns="[^"]+"', '', xml.text, count=1)
+    #         file.write(text)
+    #
+    #     root = et.parse('temp.xml').getroot()
+    #     for dp in root.findall('dependencies/dependency'):
+    #         group = dp.find('groupId').text
+    #         artifact = dp.find('artifactId').text
+    #         version = dp.find('version').text
--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+from sys import argv
 from graph_builder import GraphBuilder
+import xml.etree.ElementTree as et
+
+
+def main():
+    if len(argv) > 1:
+        config_path = argv[1]
+        try:
+            root = et.parse(config_path).getroot()
+            p_uml = root.find('PlantUml').text
+            package = dict()
+            package['group'] = root.find('group').text
+            package['artifact'] = root.find('artifact').text
+            package['version'] = root.find('version').text
+            code = root.find('code').text
+        except FileNotFoundError:
+            print('Не удаётся открыть конфигурационный файл')
+            return
+
+        builder = GraphBuilder()
+        builder.build_graph(code, package)
+
+    else:
+        print('Не указан путь к конфигурационному файлу')
+
 
 if __name__ == '__main__':
-    a = GraphBuilder()
-    # url = 'https://repo1.maven.org/maven2/org/springframework/spring-webmvc/6.1.6/spring-webmvc-6.1.6.pom'
-    # a.get_pom(url)
+    main()
